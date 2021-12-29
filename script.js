8e17718e563bd65a36bc36b0db1a33fd47d91214
Vadym <kb201_dvo@student.ztu.edu.ua>
2021-12-29 15:59:11 +0200

    not working but work

---
+++ script.js
@@ -0,0 +1,24 @@
+let deadline = new Date('Dec 31 2021 00:00:00');
+
+function count (){
+    let now = new Date();
+    gap = deadline - now;
+
+    let days = Math.floor(gap / 1000 / 60 / 60 / 24);
+    let hours = Math.floor(gap / 1000 / 60 / 60) % 24;
+    let minutes = Math.floor(gap / 1000 / 60) % 60;
+    let seconds = Math.floor(gap / 1000) % 60;
+
+    if(gap < 0){
+        days = days + 365;
+    }else{
+        document.getElementById('days__info').innerText = days + ' Дней';
+        document.getElementById('hours__info').innerText = hours + ' Часов';
+        document.getElementById('minutes__info').innerText = minutes + ' Минут';
+        document.getElementById('seconds__info').innerText = seconds + ' Секунд';
+    }
+    
+}
+
+
+setInterval(count, 1000);
